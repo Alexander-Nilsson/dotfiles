4b1dc37bc8861328a1e569734785376cdae4519f
Alexander Nilsson <alenilsson7@gmail.com>
2025-04-04 11:46:50 +0800

    updt binds to open applications in specific workspace

--- .config/hypr/scripts/togglewaybar.sh
+++ .config/hypr/scripts/togglewaybar.sh
@@ -1,35 +1,7 @@
 #!/usr/bin/env bash
 
-hidden_on_workspace=$1
-
-hide() {
-  killall -SIGUSR1 waybar && touch /tmp/waybar_is_hidden
+toggle_waybar() {
+  killall -SIGUSR1 waybar
 }
 
-show() {
-  if [ -f /tmp/waybar_is_hidden ]; then
-    killall -SIGUSR1 waybar && rm /tmp/waybar_is_hidden
-  fi
-}
-
-sleep 1
-
-current_workspace=$(hyprctl activeworkspace | awk 'match($0, /ID ([0-9]+)/, id){print id[1]}')
-[ $hidden_on_workspace -eq $current_workspace ] && hide
-
-#socket_path="/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
-socket_path="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
-
-if [ -z "$socket_path" ]; then
-  echo "Error: Hyprland socket not found. Is Hyprland running?"
-  exit 1
-fi
-
-socat -u "UNIX-CONNECT:$socket_path" STDOUT | while read -r event; do
-  case "$event" in
-  "workspace>>$hidden_on_workspace") hide ;;
-  "workspace>>"*) show ;;
-  esac
-done
-
-show
+toggle_waybar
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- maping $ to ¤
+vim.api.nvim_set_keymap("n", "¤", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "¤", "$", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("o", "¤", "$", { noremap = true, silent = true })
