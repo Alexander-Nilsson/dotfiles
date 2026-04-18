48e01729f5977b4bd557b40b62e2a20eb8832435
Alexander Nilsson <alenilsson7@gmail.com>
2026-04-18 12:36:21 +0700

    updt to awww instead of swww

--- .config/hypr/scripts/wallpaper-restore.sh
+++ .config/hypr/scripts/wallpaper-restore.sh
@@ -2,10 +2,10 @@
 
 LAST="${HOME}/.cache/matugen/last-wallpaper"
 
-# Wait for swww daemon to be ready
+# Wait for awww daemon to be ready
 sleep 1
 
 if [ -f "$LAST" ] && [ -f "$(cat "$LAST")" ]; then
-  swww img "$(cat "$LAST")" --transition-type fade --transition-duration 1
+  awww img "$(cat "$LAST")" --transition-type fade --transition-duration 1
   matugen --source-color-index 0 image "$(cat "$LAST")"
 fi
